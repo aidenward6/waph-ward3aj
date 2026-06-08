8969281daec698e3c0b1ccb275e146e668ffe06f
Aiden Ward <126358242+aidenward6@users.noreply.github.com>
2026-06-07 21:37:12 -0400

    Refactor AJAX response handling in script.js
    Updated response handling to use textContent instead of innerHTML.

--- waph-ward3aj/labs/lab2/script.js
+++ waph-ward3aj/labs/lab2/script.js
@@ -7,15 +7,12 @@ function sendAjax() {
     var input = document.getElementById("ajax-input").value;
     var xhttp = new XMLHttpRequest();
     
-    // Define what to do when the server responds
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Display the response from echo.php inside your <div>
-            document.getElementById("ajax-result").innerHTML = this.responseText;
+            document.getElementById("ajax-result").textContent = this.responseText;
         }
     };
     
-    // Construct the GET request
     xhttp.open("GET", "/echo.php?data=" + encodeURIComponent(input), true);
     xhttp.send();
 }
