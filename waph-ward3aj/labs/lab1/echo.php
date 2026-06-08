df21da0dfa3472e23bc2b351a8b60b6af74f10e2
Aiden Ward <126358242+aidenward6@users.noreply.github.com>
2026-06-07 21:57:39 -0400

    security changes

--- waph-ward3aj/labs/lab1/echo.php
+++ waph-ward3aj/labs/lab1/echo.php
@@ -1,4 +1,13 @@
 <?php
 echo "<h1>Hello from PHP</h1>";
-echo "Name: " .
-htmlspecialchars($_GET['name']);
+
+// check for input before processing
+if (isset($_GET['name']) && trim($_GET['name']) !== '') {
+    $name = $_GET['name'];
+    
+    // escape html tags to prevent xss usign utf-8 encoding
+    echo "Name: " . htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+} else {
+    echo "Name: Please provide a valid name.";
+}
+?>
