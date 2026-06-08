fb29d58639ff7ffec67f5f80cf61cbdf640e170e
Aiden Ward <126358242+aidenward6@users.noreply.github.com>
2026-06-07 21:51:06 -0400

    Original

--- waph-ward3aj/labs/lab1/echo.php
+++ waph-ward3aj/labs/lab1/echo.php
@@ -1,7 +1,4 @@
 <?php
 echo "<h1>Hello from PHP</h1>";
-if (isset($_GET['name'])) {
-    $name = $_GET['name'];
-    echo "Name: " . htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-}
-?>
+echo "Name: " .
+htmlspecialchars($_GET['name']);
